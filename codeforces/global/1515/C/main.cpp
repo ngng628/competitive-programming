b61b6d73de86fe38fd72c7bbb75c04daeb8aa1de
ngng628 <e16-jahx@kure.kosen-ac.jp>
2021-05-03 15:01:40 +0900

    :bar_chart: ぐろふぉ解いた！

---
+++ codeforces/global/1515/C/main.cpp
@@ -0,0 +1,84 @@
+# include <bits/stdc++.h>
+# ifndef ngng628_library
+# define ngng628_library
+# define int long long
+# define float long double
+# define fi first
+# define se second
+# define rep(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
+# define reps(i,n) for(int i=1, i##_len=(n); i<=i##_len; ++i)
+# define rrep(i,n) for(int i=(int)(n)-1; i>=0; --i)
+# define rreps(i,n) for(int i=(int)(n); i>0; --i)
+# define repr(i,b,e) for(int i=(b), i##_len=(e); i<i##_len; ++i)
+# define reprs(i,b,e) for(int i=(b), i##_len=(e); i<=i##_len; ++i)
+# define all(x) std::begin(x), std::end(x)
+# define rall(x) std::rbegin(x), std::rend(x)
+# define pb push_back
+# define eb emplace_back
+# define len(x) (int)(x).size()
+using namespace std;
+template<class T> using vec = vector<T>;
+using pii = pair<int, int>;
+using vi = vec<int>;
+using vvi = vec<vi>;
+using db = deque<bool>;
+using ddb = deque<db>;
+using vs = vec<string>;
+constexpr int INF = (1LL<<62)-(1LL<<31);
+constexpr float EPS = 1e-10;
+template<class T> istream& operator >>(istream& is, vec<T>& v) { for (auto& x : v) is >> x; return is; }
+template<class T, std::size_t N> istream& operator >>(istream& is, array<T, N>& v) { for (auto& x : v) is >> x; return is; }
+template<class T, class U> istream& operator >>(istream& is, pair<T, U>& p) { return is >> p.fi >> p.se; }
+template<class T> string join(const vec<T> &v){ stringstream s; rep (i, len(v)) s << ' ' << v[i]; return s.str().substr(1); }
+template<class T> ostream& operator <<(ostream& os, const vec<T>& v){ if (len(v)) os << join(v); return os; }
+template<class T> ostream& operator <<(ostream& os, const vec<vec<T>>& v){ rep (i, len(v)) if (len(v[i])) os << join(v[i]) << (i-len(v)+1 ? "\n" : ""); return os; }
+template<class T, class U> ostream& operator <<(ostream& os, const pair<T, U>& p){ return os << p.fi << " " << p.se; }
+template<class T, class U, class V> ostream& operator <<(ostream& os, const tuple<T, U, V>& t){ return os << get<0>(t) << " " << get<1>(t) << " " << get<2>(t); }
+void print(){ cout << "\n"; }
+template<class T, class... A>void print(const T& v, const A&...args){ cout << v; if (sizeof...(args)) cout << " "; print(args...); }
+void eprint() { cerr << "\n"; }
+template<class T, class... A>void eprint(const T& v, const A&...args){ cerr << v; if (sizeof...(args)) cerr << " "; eprint(args...); }
+# define drop(...) { print(__VA_ARGS__); return; }
+template<class T> constexpr bool chmax(T &a, const T& b){ return a < b && (a = b, true); }
+template<class T> constexpr bool chmin(T &a, const T& b){ return a > b && (a = b, true); }
+constexpr int ctoi(const char c){ return '0' <= c and c <= '9' ? (c - '0') : -1; }
+struct Setup_io { Setup_io(){ ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0), cerr.tie(0); cout << fixed << setprecision(15); } } setup_io;
+# endif  // ngng628_library
+
+struct Solver {
+   void solve() {
+      int n, m, x;
+      cin >> n >> m >> x;
+      vi h(n);
+      cin >> h;
+
+      priority_queue<pii, vec<pii>, greater<pii>> pq;
+      rep (i, m) pq.emplace(0, i);
+      vi ans(n);
+      rep (i, n) {
+         auto [height, idx] = pq.top(); pq.pop();
+         height += h[i];
+         ans[i] = idx + 1;
+         pq.emplace(height, idx);
+      }
+
+      vi check(m);
+      rep (i, n) check[ans[i] - 1] += h[i];
+      if (*max_element(all(check)) - *min_element(all(check)) > x) {
+         print("NO");
+      }
+      else {
+         print("YES");
+         print(ans);
+      }
+   }
+};
+
+int32_t main() {
+   int t;
+   cin >> t;
+   while (t--) {
+      Solver solver;
+      solver.solve();
+   }
+}
