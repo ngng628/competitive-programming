2e962fd7ea92ab633b6d8b0d064b16feb4c42943
ngng628 <ngng628.kyopro@gmail.com>
2022-10-25 14:35:31 +0900

    :horse: 解いた

--- atcoder/ABC/ABC274/D/main.cr
+++ atcoder/ABC/ABC274/D/main.cr
@@ -1,9 +1,30 @@
-# ○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．
-def int(b = 0); read_line.to_i64 + b end
-def ints(b = 0); read_line.split.map{ |x| x.to_i64 + b } end
-def str; read_line.chomp end
-macro chmin(a, b); {{a}} = Math.min({{a}}, {{b}}) end
-macro chmax(a, b); {{a}} = Math.max({{a}}, {{b}}) end
-OO = (1_i64<<62)-(1_i64<<31)
-# ○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．
+def ok(seq, target)
+  offset = 10**4
+  dp = Array.new(seq.size + 1){ Array.new(10**5){ false } }
+  dp[0][offset] = true
+  seq.each_with_index do |ai, i|
+    (ai..10**4 + offset).each do |k|
+      dp[i + 1][k + ai] |= dp[i][k]
+      dp[i + 1][k - ai] |= dp[i][k]
+    end
+  end
+  dp[seq.size][target + offset]
+end
 
+n, gx, gy = read_line.split.map(&.to_i)
+at = read_line.split.map(&.to_i)
+a0 = at[0]
+a = at[1..]
+gx -= a0
+
+xs = [] of Int32
+ys = [] of Int32
+a.each_with_index do |ai, i|
+  if i.odd?
+    xs << ai
+  else
+    ys << ai
+  end
+end
+
+puts ok(xs, gx) && ok(ys, gy) ? "Yes" : "No"
