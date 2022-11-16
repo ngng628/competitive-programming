8d6983c480b3032887d11943e06966ff6c13d0b0
ngng628 <ngng628.kyopro@gmail.com>
2022-11-17 03:13:09 +0900

    :horse: 解いた

--- atcoder/ABC/ABC266/E/main.cr
+++ atcoder/ABC/ABC266/E/main.cr
@@ -8,3 +8,18 @@ OO = (1_i64<<62)-(1_i64<<31)
 # ○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．○。．
 
 n = int
+
+cache = Array(Float64?).new(n + 1){ nil }
+dp = uninitialized Proc(Int64, Float64)
+dp = ->(i : Int64) do
+  return 0.0 if i == 0
+  return cache[i].not_nil! unless cache[i].nil?
+
+  cache[i] = (1..6).sum do |j|
+    Math.max(dp.call(i - 1), j.to_f64) / 6.0
+  end
+
+  cache[i].not_nil!
+end
+
+puts dp.call(n)
